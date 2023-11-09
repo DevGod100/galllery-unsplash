194a76110e0ad8e1caec74020e8ec90c02028dfa
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2023-11-09 19:52:47 +0200

    navbar - cannot access User Avatar img

--- components/UI/NavBar/NavBar.js
+++ components/UI/NavBar/NavBar.js
@@ -1,17 +1,64 @@
 'use client'
 import { signIn, signOut } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = ({user}) => {
   return (
-    <div style={{display: 'flex', gap: 50}}>
-      {
-        user
-        ? <button onClick={signOut}>{user.name} Logout</button>
-        :  <button onClick={() => signIn('google')}>Login</button>
-      }
-    </div>
+     <nav>
+      <div className='container navbar'>
+        <Link href='/' className='nav_logo'>
+          <b>N</b><span>ovem</span>
+        </Link>
+        <div className='nav_search'>
+
+        </div>
+
+        <div className='nav_menu'>
+          {
+            !user
+            ? <>
+            <button className='btn btn_login' onClick={() => signIn('google')}>
+              <i className='material-icons'>person</i>
+              <span>Log in</span>
+            </button>
+
+            <button className='btn btn_icon' onClick={() => signIn('google', {callbackUrl: '/upload'})}>
+              <i className='material-icons-outlined'>file_upload</i>
+              <span>Submit a photo</span>
+            </button>
+            </>
+
+            : <>
+            <Link href='/upload' className='btn btn_icon'>
+            <i className='material-icons-outlined'>file_upload</i>
+              <span>Submit a photo</span>
+            </Link>
+
+            <Link href={`/profile/${user?._id}`} className='avatar'>
+            <Image src={user?.avatar} alt={user?.name}
+            width={40} height={40} sizes='25vw'
+            />
+            </Link>
+            <button onClick={signOut}>{user.name} Logout</button>
+            </>
+          }
+        </div>
+
+        </div>
+     </nav>
   )
 }
 
 export default NavBar
+
+
+
+{/* <div style={{display: 'flex', gap: 50}}>
+{
+  user
+  ? <button onClick={signOut}>{user.name} Logout</button>
+  :  <button onClick={() => signIn('google')}>Login</button>
+}
+</div> */}
