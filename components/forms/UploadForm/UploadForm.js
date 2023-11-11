0478cb1bc8fdbfb37a090d39732c70633518ef9f
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2023-11-11 18:11:00 +0200

    img upload style

--- components/forms/UploadForm/UploadForm.js
+++ components/forms/UploadForm/UploadForm.js
@@ -1,10 +1,18 @@
 'use client'
+import { useRef } from 'react'
 import './UploadForm.css'
+import Image from 'next/image'
 
 const UploadForm = ({setFiles}) => {
   const formRef = useRef()
   return (
-    <div>UploadForm</div>
+    <form action='' className='upload_form'>
+      <input type='file' id='upload' accept='.png, .jpg' multiple hidden/>
+      <label htmlFor='upload' className='upload_form_label'>
+         <Image  src={'/empty.png'} height={40} width={130} sizes='25vw' 
+         style={{width: 136, height: 96}}/>
+      </label>
+    </form>
   )
 }
 
